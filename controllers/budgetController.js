25f256af5a53c04b70a718487aaf6cf132706781
Hotblueice777 <joeyqi777@gmail.com>
2024-05-05 18:18:31 -0600

    modify update budget function, test successfully, and the front-end is successfully connected

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -14,6 +14,28 @@ const getBudget = (req, res) => {
 };
 
 // renew current budget
+// renew current budget
+const updateBudget = (req, res) => {
+  console.log('Received update request with data:', req.body);
+  const { budget } = req.body; 
+
+  // make sure number is valid
+  if (!budget || isNaN(parseFloat(budget))) {
+    return res.status(400).json({ message: "Invalid budget amount provided" });
+  }
+
+  try {
+    const config = { budget: parseFloat(budget) };
+    fs.writeFileSync(configPath, JSON.stringify(config), 'utf-8');
+    res.status(200).json({ message: "Budget updated successfully", budget: config.budget });
+  } catch (error) {
+    console.error("Error updating budget:", error);
+    res.status(500).json({ message: "Failed to update budget" });
+  }
+};
+
+
+/*
 const updateBudget = (req, res) => {
   console.log('Received update request with data:', req.body);
   const { newBudget } = req.body;
@@ -32,6 +54,8 @@ const updateBudget = (req, res) => {
     res.status(500).json({ message: "Failed to update budget" });
   }
 };
+
+*/
 
 // getBudgetForCurrentMonth
 const getBudgetForCurrentMonth = async () => {
